243e89897f12f7f98d130d873bdf927378ee2a1b
ankairfan <lux2vintage@gmail.com>
2022-02-22 11:58:23 +0300

    DevDataGrid componenti, ICoreDataGridService, DevBaseViewer oluşturuldu.

---
+++ src/AnkaYazilim.Blazor.Core/Services/ICoreDataGridService.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace AnkaYazilim.Blazor.Core.Services;
+
+public interface ICoreDataGridService<TDataGridItem>
+{
+    public ComponentBase DataGrid { get; set; }
+    public IList<TDataGridItem> DataSource { get; set; }
+    public TDataGridItem SelectedItem { get; set; }
+    public IEnumerable<TDataGridItem> SelectedItems { get; set; }
+    public bool ShowFilterRow { get; set; }
+    public bool ShowGroupPanel { get; set; }
+}
